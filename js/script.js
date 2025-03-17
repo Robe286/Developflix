05f34c25856be24c40c4233a2a15745dea1821f9
Roberto Blasco~ <roberza2202@gmail.com>
2025-03-17 17:39:31 +0100

    creacion-Nodos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 import peliculas from './peliculas.js'
 
-//const accion = peliculas.getElementById('genre_ids');
-console.log(peliculas);
+//Trabajo en el DOM
 
+// - Capturar los div de cada género:
+
+const generoAccion = document.getElementById('genero-28');
+const generoThriller = document.getElementById('genero-53');
+const generoAventura = document.getElementById('genero-12');
+
+
+//Crear nodos para título película y imagen:
+
+//Crear nodos img y h3
+const elementImagen = document.createElement('img');
+const tituloPelicula = document.createElement('h3');
+console.log(tituloPelicula);
+
+//Agregar los elemento creados a los section:
+generoAccion.appendChild(elementImagen);
+generoThriller.appendChild(elementImagen);
+
+console.log(generoAccion);
+
+
+//const Accion = peliculas.filter((pelicula) => pelicula.genre_ids.includes(28));
